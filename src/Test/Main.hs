d558d5f47fc552be643da062b8919bee8d5f1f22
agomezl <alegomez544@gmail.com>
2015-10-06 16:46:27 -0500

    Minimal testing added

---
+++ src/Test/Main.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE UnicodeSyntax #-}
+--------------------------------------------------------------------------------
+-- File   : Main
+-- Author : Alejandro Gomez
+-- Date   : Tue Oct  6 16:43:59 2015
+-- Description : Tests for tstp2agda
+--------------------------------------------------------------------------------
+-- Change log :
+
+--------------------------------------------------------------------------------
+
+module Main where
+
+main ∷ IO ()
+main = do
+  putStrLn "TODO: tests"
