b58918d13fea6e16406e954c81283627de88233c
agomezl <alegomez544@gmail.com>
2015-11-25 02:33:09 -0500

    Data.Util documentation added

--- src/Util.hs
+++ src/Util.hs
@@ -16,7 +16,19 @@
 #define MIN_VERSION_base(a,b,c) 1
 #endif
 -- Assume we are using the newest versions when using ghci without cabal
-module Util where
+module Util (
+            -- * Spaced concatenation
+              (▪)
+            , BShow(..)
+            -- * Printing with indentation
+            , printInd
+            , putStrLnInd
+            -- * List manipulation
+            , unique
+            , swapPrefix
+            -- * Others
+            , agdafy
+            ) where
 
 import Data.Foldable      (toList)
 import Data.Set           (fromList)
@@ -24,16 +36,32 @@ import Data.List          (isPrefixOf)
 
 infixr 4 ▪
 
+-- | '"foo" ▪ "bar"' = '"foo bar"'. its main use is to simplify the
+-- concatenation of printable types separated by spaces.
 (▪) ∷ forall a b. (BShow a, BShow b) ⇒ a → b → String
 (▪) α₁ α₂ = βshow α₁ ++ " " ++ βshow α₂
+
+-- | 'BShow' fixes 'Show' 'String' instance behavior @length "foo" ≠
+-- length (show "foo")@ with two new instances (and overlapped)
+-- instances for 'String' and 'Show' 'a'.
 
 class BShow a where
     βshow ∷ a -> String
 
+-- TODO: fix overlapping mess
+
+#if MIN_VERSION_base(4,8,0)
 instance {-# OVERLAPPING #-} BShow String where
+#else
+instance BShow String where
+#endif
     βshow a = a
 
+#if MIN_VERSION_base(4,8,0)
 instance {-# OVERLAPPING #-} BShow Char where
+#else
+instance BShow Char where
+#endif
     βshow a = [a]
 
 #if MIN_VERSION_base(4,8,0)
@@ -43,23 +71,36 @@ instance Show a ⇒ BShow a where
 #endif
   βshow = show
 
+-- | Removes duplicate elements of a list.
 unique ∷ (Ord a) ⇒ [a] → [a]
 unique a = toList . fromList $ a
 
+-- | 'printInd' @i b@, prints a with @b@ level of indentation @i@.
 printInd ∷ (Show a) ⇒ Int → a → IO ()
 printInd ind a = putStr spaces >> print a
     where spaces = replicate ind ' '
 
+-- | 'printInd' @i str@, prints a with @str@ level of indentation @i@.
 putStrLnInd ∷ Int → String → IO ()
 putStrLnInd ind a = putStr spaces >> putStrLn a
     where spaces = replicate ind ' '
 
 
-swapPrefix ∷ String → String → String → String
+-- | 'swapPrefix' @a b str@, replaces prefix @a@ in @str@ with @b@
+-- checking that @a@ is a prefix of @str@.
+swapPrefix ∷ Eq a ⇒
+             [a] -- ^ Current prefix
+           → [a] -- ^ Replacement
+           → [a] -- ^ List to be replaced
+           → [a] -- ^ Resulting list
 swapPrefix a b str
     | a `isPrefixOf`str = b ++ drop (length a) str
     | otherwise         = str
 
+-- | Metis identifiers usually contain '_' characters which are invalid
+-- in Agda, 'agdafy' replaces @normalize_0_0@ with
+-- @normalize-0-0@. This is mostly used inside the Happy parser every
+-- time an 'AtomicWord' is created.
 agdafy ∷ String → String
 agdafy = map repl
     where repl '_' = '-'
