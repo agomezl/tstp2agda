11117faebc554b546988a646d4913df237cd8df7
agomezl <alegomez544@gmail.com>
2015-12-03 01:58:53 -0500

    new test script added

---
+++ run-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+TSTP_EXAMPLES=examples/proof
+TEST_DIR=$$.test
+SUCCESS=0
+
+function do_test {
+    MOD=$(basename $1 | cut -d. -f 1)
+    FILE=${MOD}.agda
+    cabal run -- -f $1 -o ${TEST_DIR}/${FILE} -m ${MOD}
+    agda -i src/ -i ${TEST_DIR}/ ${TEST_DIR}/${FILE} \
+        && ((++SUCCESS)) || echo "FAILURE with ${MOD}"
+    echo
+}
+
+function cleanup {
+    rm -fr ${TEST_DIR}
+}
+
+trap cleanup EXIT SIGINT SIGTERM
+
+mkdir -p ${TEST_DIR}
+
+for TEST in ${TSTP_EXAMPLES}/*.tstp
+do
+    do_test ${TEST}
+done
+
+[ ${SUCCESS} -lt 2 ] && exit 1 || exit 0
